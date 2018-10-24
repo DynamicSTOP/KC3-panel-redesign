d4d42a41da4f4b5dedcda75a0da87a10ddd7f8d2
Leonid Babikov <komsldt@gmail.com>
2018-10-24 21:31:37 +0300

    options panel

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="box">
-        <div id="panel" :class="{hidden:!optionsPanelVisible}">
+        <div id="panel" :class="{hidden:optionsPanelVisible}">
             <panel-header></panel-header>
             <panel-main></panel-main>
             <div id="panel-help"></div>
@@ -8,6 +8,7 @@
         <div id="twitter"><a href="https://twitter.com/tsekino0530" rel="noopener" target="_blank">https://twitter.com/tsekino0530</a>
         </div>
         <panel-side></panel-side>
+        <panel-options :class="{hidden:!optionsPanelVisible}"></panel-options>
     </div>
 </template>
 
@@ -17,6 +18,7 @@
     import PanelHeader from '@/components/PanelHeader'
     import PanelSide from '@/components/PanelSide'
     import PanelMain from '@/components/PanelMain'
+    import PanelOptions from '@/components/PanelOptions'
     import '@/sass/main.scss'
 
     export default {
@@ -24,6 +26,6 @@
         computed: {
             ...mapGetters(['optionsPanelVisible'])
         },
-        components: {PanelHeader, PanelMain, PanelSide}
+        components: {PanelHeader, PanelMain, PanelSide, PanelOptions}
     }
 </script>
---
+++ src/components/PanelOptions.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="panel-options">
+        <div class="title head">OPTIONS</div>
+        <span class="close-options" @click="toggleOptions"></span>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+
+    export default {
+        name: 'PanelOptions',
+        computed: {
+            ...mapGetters(['options'])
+        },
+        methods: {
+            toggleOptions() {
+                this.$store.commit('toggleOptionsPanel');
+            }
+        },
+        data() {
+            return {};
+        }
+    }
+</script>
